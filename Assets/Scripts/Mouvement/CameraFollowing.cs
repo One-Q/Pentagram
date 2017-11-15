173756ae1447b8479bb5a60753d1132886286b58
Gilmart Quentin <quentin.gilmart@student.vinci.be>
2017-11-15 15:14:46 +0100

    test

---
+++ Assets/Scripts/Mouvement/CameraFollowing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowing : MonoBehaviour {
+	
+		public GameObject obj;       
+
+		private Vector3 offset;    
+
+		// Use this for initialization
+		void Start () 
+		{
+		offset = transform.position - obj.transform.position;
+		}
+
+		void Update () 
+		{
+			transform.position = obj.transform.position + offset;
+		}
+	}
---
+++ Assets/Scripts/Mouvement/Homing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Homing : MonoBehaviour {
+
+	/*public GameObject  target;
+	public string targetTag;
+	public int rotationSpeed;
+	public int speed;
+
+	// Use this for initialization
+	void OnEnable () {
+		if (!target) {
+			target = GameObject.FindGameObjectWithTag (targetTag);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target) {
+			Vector3 direction = target.transform.position - transform.position;
+			float angleToTarget = Vector3.Angle (t
+			Vector3 velocity = transform.position;
+			velocity *= speed * Time.deltaTime;
+			transform.position += velocity;
+
+		}
+	}*/
+}
