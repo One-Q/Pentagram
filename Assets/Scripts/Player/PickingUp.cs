0aa68c0afe14ff20f8471647ed3207f45a5e509e
Gilmart Quentin <quentin.gilmart@student.vinci.be>
2017-11-22 14:49:46 +0100

    Item in inventory

--- Assets/Scripts/Player/PickingUp.cs
+++ Assets/Scripts/Player/PickingUp.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PickingUp : MonoBehaviour {
 
 	public int nbSlots = 20;
+	public GameObject inventoryCanvas;
+	public Transform itempSlotPrefab;
 	public Transform inventorySlots;
 
 	void OnTriggerEnter(Collider col){
@@ -15,7 +17,14 @@ public class PickingUp : MonoBehaviour {
 
 	public void PutInInventory(GameObject item){
 		if (inventorySlots.childCount < nbSlots) {
-			
+			Transform newItem = Instantiate (itempSlotPrefab, Vector3.zero, Quaternion.identity) as Transform;
+			newItem.SetParent (inventorySlots, false);
+			ItemSlot itemInventory = newItem.GetComponent<ItemSlot> ();
+			ItemScene itemScene = item.GetComponent<ItemScene> ();
+			itemInventory.itemDescription = itemScene.itemDescription;
+			itemInventory.itemID = itemScene.itemID;
+			itemInventory.itemType = itemScene.itemType;
+			itemInventory.itemSprite = itemScene.itemSprite;
 		} else {
 			Debug.Log ("Pas de place");
 		}
