c763c691e4144b4f0ad2bb920ce2e84482dac578
nicolasBertolini <nicolas.bertolini@student.vinci.be>
2017-12-06 13:48:03 +0100

    GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public List<Transform> wayPointsForAI;
+    private StateController m_StateController;
+
+    public GameObject m_Instance;
+
+
+
+    // Use this for initialization
+    void Start () {
+        SetupAI(wayPointsForAI);
+
+    }
+	
+	
+    public void SetupAI(List<Transform> wayPointList)
+		{
+			m_StateController = m_Instance.GetComponent<StateController> ();
+			m_StateController.SetupAI (true, wayPointList);
+		}
+
+}
