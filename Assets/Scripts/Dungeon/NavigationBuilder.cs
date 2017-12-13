095927e4c79e58b1318c359b6ed27e48713a0592
Gilmart Quentin <quentin.gilmart@student.vinci.be>
2017-12-13 10:50:37 +0100

    Dungeon

---
+++ Assets/Scripts/Dungeon/NavigationBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationBuilder : MonoBehaviour {
+
+	public NavMeshSourceTag[] sources;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+}
