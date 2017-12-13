48904c8e7ed63f241db296c1169ac509925b35a9
Gilmart Quentin <quentin.gilmart@student.vinci.be>
2017-12-13 15:41:27 +0100

    Player Texture

--- Assets/Scripts/Mouvement/Agent.cs
+++ Assets/Scripts/Mouvement/Agent.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 public class Agent : MonoBehaviour {
 
 	NavMeshAgent agent;
+	public Animator animator;
 
 	// Use this for initialization
 	void Start () {
@@ -16,12 +17,25 @@ public class Agent : MonoBehaviour {
 	void Update () {
 		RaycastHit hit;
 		if (Input.GetMouseButton(0)) {
+			animator.SetBool("Moving", true);
+			animator.SetBool("Running", true);
 			//agent.isStopped = true;
-			agent.ResetPath ();
+			//agent.ResetPath ();
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			if (Physics.Raycast(ray, out hit))
 				agent.SetDestination(hit.point);
 
 		}
+		if (agent.remainingDistance == 0) {
+			animator.SetBool("Moving", false);
+			animator.SetBool("Running", false);
+		}
+		if (Input.GetKey ("z")) {
+			agent.isStopped = true;
+			agent.ResetPath ();
+			animator.SetBool("Moving", false);
+			animator.SetBool("Running", false);
+		}
+		agent.isStopped = false;
 	}
 }
