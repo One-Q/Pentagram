20ab2bb580be4fb72fc5d10c64f1947621bc4bd0
Robert <woronkorobert96@gmail.com>
2017-12-17 14:35:49 +0100

    Update Camera Level

---
+++ Assets/Scripts/Camera/PositionUpdaterCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionUpdaterCamera : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		//Camera follow on X but don't rotate on Y
+		transform.rotation = new Quaternion (0.7f,0f,0f,0.7f);
+	}
+}
